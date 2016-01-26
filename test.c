c07bd8e80d48d0ae271f5627c206ee4f23f3611d
Brandon Milton <bmilton@ucsd.edu>
2016-01-26 10:14:05 -0800

    Fixed process 0 index issue. Added slack calculation to proportional tests. Added new proportional test.

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
  * pa2 tester
  * bmilton
  * 25 jan 2016
+ * latest update: 26 jan 2016
  */
 #include "mykernel2.h"
 #include "aux.h"
@@ -13,16 +14,21 @@
 
 int totalFailCounter = 0;
 
+int inSlackRange(int expected, int actual) {
+  double slack = expected * 0.10;
+  return abs(actual - expected) <= slack;
+}
+
 int test_fifo_normal(int numprocs) {
   int failCounter = 0;
   SetSchedPolicy(FIFO);
   InitSched();
   int proc;
-  for (proc = 0; proc < numprocs; proc++) {
+  for (proc = 1; proc <= numprocs; proc++) {
     StartingProc(proc);
   }
 
-  for (proc = 0; proc < numprocs; proc++) {
+  for (proc = 1; proc <= numprocs; proc++) {
     int next = SchedProc();
     if (next != proc) {
       Printf("FIFO ERR: Received process %d but expected %d", next, proc);
@@ -42,11 +48,11 @@ int test_lifo_normal(int numprocs) {
   InitSched();
   int proc;
 
-  for (proc = 0; proc < numprocs; proc++) {
+  for (proc = 1; proc <= numprocs; proc++) {
     StartingProc(proc);
   }
 
-  for (proc = numprocs - 1; proc >= 0; proc--) {
+  for (proc = numprocs; proc > 0; proc--) {
     int next = SchedProc();
     if (next != proc) {
       Printf("LIFO ERR: Received process %d but expected %d", next, proc);
@@ -66,26 +72,26 @@ int test_rr_normal(int numprocs) {
   InitSched();
   int prevproc, nextproc, iter, i;
 
-  for (prevproc = 0; prevproc < numprocs; prevproc++) {
+  for (prevproc = 1; prevproc <= numprocs; prevproc++) {
     StartingProc(prevproc);
   }
 
   prevproc = SchedProc();
   for (iter = 0; iter < 5; iter++) {
-    for (i = 0; i < numprocs; i++) {
+    for (i = 1; i <= numprocs; i++) {
       nextproc = SchedProc();
-      if ((prevproc + 1) % numprocs != nextproc &&
-          ((prevproc - 1) < 0 ? numprocs - 1 : prevproc - 1) != nextproc) {
+      if (((prevproc + 1) % numprocs) + 1 != nextproc &&
+          ((prevproc - 1) < 1 ? numprocs  : prevproc - 1) != nextproc) {
         Printf("ROUND ROBIN ERR: Encountered process %d\n when expecting %d or %d\n", 
-            nextproc, (prevproc+1)%numprocs, 
-            ((prevproc - 1) < 0 ? numprocs - 1 : prevproc - 1));
+            nextproc, ((prevproc+1)%numprocs) + 1, 
+            ((prevproc - 1) < 1 ? numprocs  : prevproc - 1));
         failCounter++;
       }
       prevproc = nextproc;
     }
   }
 
-  for (prevproc = 0; prevproc < numprocs; prevproc++)
+  for (prevproc = 1; prevproc <= numprocs; prevproc++)
     EndingProc(prevproc);
 
   totalFailCounter += failCounter;
@@ -102,32 +108,97 @@ int test_proportional_normal(int numprocs) {
 
   int proportions[numprocs];
   int counts[numprocs];
-  for (i = 0; i < numprocs; i++) {
+  for (i = 1; i <= numprocs; i++) {
     StartingProc(i);
     proportions[i] = (rand() % (100 / numprocs)) + 1;
     counts[i] = 0;
   }
 
-  for (i = 0; i < numprocs; i++) {
-    if (MyRequestCPUrate(i, proportions[i]) == -1)
+  for (i = 1; i <= numprocs; i++) {
+    if (MyRequestCPUrate(i, proportions[i]) == -1) {
       failCounter++;
+      Printf("PROPORTIONAL ERR: Process requested %d%% CPU and MyRequestCPUrate wrongly returned -1\n",
+          proportions[i]);
+    }
   }
 
   for (i = 0; i < 100; i++) {
     counts[SchedProc()]++;
   }
 
-  for (i = 0; i < numprocs; i++) {
-    if (counts[i] - proportions[i] < 0) {
+  for (i = 1; i <= numprocs; i++) {
+    if (!inSlackRange(proportions[i], counts[i]) && 
+        counts[i] < proportions[i]) {
       Printf("PROPORTIONAL ERR: %d requested %d%% but received %d\n", i, 
           proportions[i], counts[i]);
       failCounter++;
     }
   }
 
-  for (i = 0; i < numprocs; i++)
+  for (i = 1; i <= numprocs; i++)
     EndingProc(i);
 
+  totalFailCounter += failCounter;
+  return failCounter;
+}
+
+int test_proportional_hog(int numprocs) {
+  int failCounter = 0;
+  SetSchedPolicy(PROPORTIONAL);
+  InitSched();
+  int i, iter;
+  int counts[numprocs];
+
+  for (i = 1; i <= numprocs; i++) {
+    StartingProc(i);
+    counts[i] = 0;
+  }
+
+  if (MyRequestCPUrate(1, 100) == -1) {
+    Printf("PROPORTIONAL ERR: A single process requested 100%% CPU and MyRequestCPURate returned -1\n");
+    failCounter++;
+  }
+
+  for (i = 2; i <= numprocs; i++) {
+    if (MyRequestCPUrate(i, 20) != -1) {
+      Printf("PROPORTIONAL ERR: Process %d requested unavailable space and MyRequestCPUrate returned 0\n",
+          i);
+      failCounter++;
+    }
+  }
+
+  for (iter = 0; iter < 100; iter++) {
+    counts[SchedProc()]++;
+  }
+
+  if (counts[1] != (100 - (numprocs - 1))) {
+    Printf("PROPORTIONAL ERR: Process 1 should have received %d%% CPU time but got %d%%\n",
+        (100 - (numprocs - 1)), counts[1]);
+    failCounter++;
+  }
+
+  for (i = 2; i <= numprocs; i++) {
+    if (counts[i] > 1) {
+      Printf("PROPORTIONAL ERR: Process %d shouldnt have received >1%% CPU time (Received %d%%) " 
+          "since process 1 requested 100%%. \n",
+          i, counts[i]);
+      failCounter++;
+    }
+    counts[i] = 0;
+  }
+
+  EndingProc(1);
+  for (iter = 0; iter < 100; iter++) {
+    counts[SchedProc()]++;
+  }
+
+  for (i = 2; i <= numprocs; i++) {
+    if (!inSlackRange(100 / (numprocs-1), counts[i])) {
+      Printf("PROPORTIONAL ERR: Process %d was expected to receive %d%% CPU (RR after no requests"
+        " for CPU were made), but received %d%%\n", i, (100/(numprocs-1)), counts[i]);
+      failCounter++;
+    }
+  }
   totalFailCounter += failCounter;
   return failCounter;
 }
@@ -146,7 +217,7 @@ void Main() {
   Printf("%d lifo failures\n", test(&test_lifo_normal));
   Printf("%d roundrobin failures\n", test(&test_rr_normal));
   Printf("%d proportional failures\n", test(&test_proportional_normal));
-  //lala
+  Printf("%d proportional2 failures\n", test(&test_proportional_hog));
 
   Printf("%d Failures\n", totalFailCounter);
   if (totalFailCounter == 0)
