2c5612772a941a95005e5f74c3f204442bb34165
Brandon Milton <bmilton@ucsd.edu>
2016-01-26 09:34:03 -0800

    Modified installer to work properly with paths

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ln test.c ../test.c
+ln $HOME/pa2/ucsd-cs120-wi16-pa2-tester/test.c $HOME/pa2/test.c
 str="test:\ttest.c aux.h umix.h mykernel2.h sys.h\n\t\$(CC) \$(FLAGS) -o test test.c mykernel2.o"
-echo $str >> ../Makefile
+echo $str >> $HOME/pa2/Makefile
