de6f343387f398dff8de567858ce1a6a3c294bf7
abuss <andrew@cryptarch.net>
2016-01-28 17:23:39 -0800

    Measure allocations over a long period rather than a 100-tick window

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ln -s $HOME/pa2/ucsd-cs120-wi16-pa2-tester/test.c $HOME/pa2/test.c
-str="test:\ttest.c aux.h umix.h mykernel2.o sys.h\n\t\$(CC) \$(FLAGS) -o test test.c mykernel2.o"
+ln -s $HOME/pa2/ucsd-cs120-wi16-pa2-tester/test.h $HOME/pa2/test.h
+ln -s $HOME/pa2/ucsd-cs120-wi16-pa2-tester/havoc.c $HOME/pa2/havoc.c
+str="test:\ttest.c test.h havoc.c aux.h umix.h mykernel2.o sys.h\n\t\$(CC) \$(FLAGS) -o test havoc.c test.c mykernel2.o"
 echo $str >> $HOME/pa2/Makefile
---
+++ test.h
@@ -0,0 +1,20 @@
+#include "mykernel2.h"
+#include "aux.h"
+#include "sys.h"
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef SLACK
+#define SLACK 1
+#endif
+
+#ifndef LOGIC_IN_HANDLETIMERINTR
+#define LOGIC_IN_HANDLETIMERINTR 0
+#endif
+
+int inSlackRange(int expected, int actual);
+int get_next_sched();
+int test_havoc();
+
+extern int totalFailCounter;
