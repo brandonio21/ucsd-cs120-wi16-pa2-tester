19d7fa3d3733cbf5d0bd479c3a62ca13e21de2c1
abuss <andrew@cryptarch.net>
2016-01-29 22:03:38 -0800

    Make havoc more configurable

--- test.h
+++ test.h
@@ -15,7 +15,7 @@
 
 int inSlackRange(int expected, int actual);
 int get_next_sched();
-int test_havoc();
+int test_havoc(int length, int p_start, int p_end, int p_storm, int p_request);
 
 extern int totalFailCounter;
 extern int verbose;
